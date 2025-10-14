d2b021bbfdeb6464c096a37bb52a673f07cd8d61
Jules Aubry <120647406+jaubry2@users.noreply.github.com>
2025-10-14 21:06:09 +0200

    feet : card pour afficher les techno et librairie

---
+++ app/components/TechnoCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-gray-100 flex flex-row rounded-md p-2 gap-4 border">
+    <div><img :src="item.icon" alt="item.title" class="w-[30px] h-[30px]"/></div>
+    <div>
+      <div>{{ item.name }}</div>
+      <div>{{ item.description }}</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type { TechnologiesCollectionItem } from "@nuxt/content";
+defineProps<{
+  item: TechnologiesCollectionItem;
+}>();
+</script>
+
+<style></style>
