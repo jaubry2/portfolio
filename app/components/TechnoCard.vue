ea3ad9c884ba30f2f7280910458866e0073357a1
Jules Aubry <120647406+jaubry2@users.noreply.github.com>
2025-10-15 19:36:25 +0200

    fix : modif utilisation des icon

--- app/components/TechnoCard.vue
+++ app/components/TechnoCard.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="bg-gray-100 flex flex-row rounded-md p-2 gap-4 border">
-    <div><img :src="item.icon" alt="item.title" class="w-[30px] h-[30px]"/></div>
+    <div class="flex items-center jsutify-center">
+      <Icon :name="item.icon" class="w-[30px] h-[30px]"/></div>
     <div>
       <div>{{ item.name }}</div>
       <div>{{ item.description }}</div>
